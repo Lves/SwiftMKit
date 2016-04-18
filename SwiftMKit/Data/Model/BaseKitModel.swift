1f66c6755eacd050be0d272e56b46d0ab5ec660c
cheny <yong.chen@jimubox.com>
2016-04-18 11:30:25 +0800

    添加baseKitModel文件

---
+++ SwiftMKit/Data/Model/BaseKitModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseKitModel.swift
+//  SwiftMKitDemo
+//
+//  Created by apple on 16/4/18.
+//  Copyright © 2016年 cdts. All rights reserved.
+//
+
+import UIKit
+
+class BaseKitModel: NSObject {
+
+}
