d377f25100212ff570dd034d70f2eb8e22d2eb5d
cdts_change <cdts_change@aliyun.com>
2016-04-18 14:35:44 +0800

    add HanekeSwift

--- SwiftMKitDemo/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ SwiftMKitDemo/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/CocoaLumberjack.framework"
+  install_framework "Pods/Haneke.framework"
   install_framework "Pods/MBProgressHUD.framework"
   install_framework "Pods/MJRefresh.framework"
   install_framework "Pods/ObjectMapper.framework"
@@ -95,6 +96,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/CocoaLumberjack.framework"
+  install_framework "Pods/Haneke.framework"
   install_framework "Pods/MBProgressHUD.framework"
   install_framework "Pods/MJRefresh.framework"
   install_framework "Pods/ObjectMapper.framework"
