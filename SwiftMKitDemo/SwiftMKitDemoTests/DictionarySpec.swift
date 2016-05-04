14f72fba1c07d7d1d52f7e340ebe4d6715cc7477
Jenny <xin.ju@jimubox.com>
2016-05-04 12:14:47 +0800

    dictionary unit test

--- SwiftMKitDemo/SwiftMKitDemoTests/DictionarySpec.swift
+++ SwiftMKitDemo/SwiftMKitDemoTests/DictionarySpec.swift
@@ -9,6 +9,8 @@
 import XCTest
 import Quick
 import Nimble
+import Foundation
+@testable import SwiftMKitDemo
 
 class DictionarySpec: QuickSpec {
 
@@ -22,9 +24,137 @@ class DictionarySpec: QuickSpec {
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDictionar() {
+        
+        describe("test dictrionary") {
+            
+            context("test string:string") {
+                var left = [
+                    "a": "123",
+                    "b": "345",
+                    "c": "123"
+                ]
+                let right = [
+                    "a": "1",
+                    "c": "2"
+                ]
+                
+                let res = left + right
+                it("should notnil") {
+                    expect(res).toNot(beNil())
+                }
+                it("should be refreshed") {
+                    expect(res["a"]).to(be(right["a"]))
+                    expect(res["b"]).to(be(left["b"]))
+                    expect(res["c"]).to(be(right["c"]))
+                }
+                
+                it("should notnil") {
+                    expect(res.stringFromHttpParameters()).toNot(beNil())
+                }
+                it("should equal to 'a=1&b=2'") {
+                    expect(res.stringFromHttpParameters()).to(equal("a=1&b=345&c=2"))
+                }
+                
+                let temp = left
+                left += right
+                it("should notnil") {
+                    expect(left).toNot(beNil())
+                }
+                it("should be refreshed") {
+                    expect(left["a"]).to(be(right["a"]))
+                    expect(left["b"]).to(be(temp["b"]))
+                    expect(left["c"]).to(be(right["c"]))
+                }
+                
+                it("should notnil") {
+                    expect(left.stringFromHttpParameters()).toNot(beNil())
+                }
+                it("should equal to 'a=1&b=2'") {
+                    expect(left.stringFromHttpParameters()).to(equal("a=1&b=345&c=2"))
+                }
+            }
+            
+            
+            
+            context("test string:double") {
+                var left = [
+                    "a": 0.6,
+                    "b": 1.2
+                ]
+                let right = [
+                    "c": 1.8,
+                    "d": 2.4
+                ]
+                
+                let res = left + right
+                it("should notnil") {
+                    expect(res).toNot(beNil())
+                }
+                it("should be refreshed") {
+                    expect(res["a"]).to(be(left["a"]))
+                    expect(res["b"]).to(be(left["b"]))
+                    expect(res["c"]).to(be(right["c"]))
+                    expect(res["d"]).to(be(right["d"]))
+                }
+                
+                it("should notnil") {
+                    expect(res.stringFromHttpParameters()).toNot(beNil())
+                }
+                it("should equal to '1=2&2=6&3=9&4=12'") {
+                    expect(res.stringFromHttpParameters()).to(equal("1=2&2=6&3=9&4=12"))
+                }
+                
+                let temp = left
+                left += right
+                it("should notnil") {
+                    expect(left).toNot(beNil())
+                }
+                it("should be refreshed") {
+                    expect(left["a"]).to(be(temp["a"]))
+                    expect(left["b"]).to(be(temp["b"]))
+                    expect(left["c"]).to(be(right["c"]))
+                    expect(left["d"]).to(be(right["d"]))
+                }
+                
+              
+                it("should notnil") {
+                    expect(left.stringFromHttpParameters()).toNot(beNil())
+                }
+                it("should equal to 'a=0.6&b=1.2&c=1.8&d=2.4'") {
+                    expect(left.stringFromHttpParameters()).to(equal("a=0.6&b=1.2&c=1.8&d=2.4"))
+                }
+            }
+            
+            
+            context("test nil") {
+                var left = [String:String]()
+                let right = [String:String]()
+                
+                let res = left + right
+                it("should betnil") {
+                    expect(res).to(beNil())
+                }
+                
+                it("should benil") {
+                    expect(res.stringFromHttpParameters()).to(beNil())
+                }
+                it("should equal to ''") {
+                    expect(res.stringFromHttpParameters()).to(equal(""))
+                }
+                
+                
+                left += right
+                it("should benil") {
+                    expect(left).to(beNil())
+                }
+                it("should equal to ''") {
+                    expect(left.stringFromHttpParameters()).to(equal(""))
+                }
+            }
+
+        }
+        
     }
 
     func testPerformanceExample() {
