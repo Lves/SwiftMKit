f727d0a4cfee2866f53189f34e627c5d12137c5f
cdts_change <cdts_change@aliyun.com>
2016-05-04 10:17:27 +0800

    添加字典相加的方法

--- SwiftMKit/Extension/Dictionary+Extension.swift
+++ SwiftMKit/Extension/Dictionary+Extension.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
-func += <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+func += <K, V> (inout left: Dictionary<K, V>, right: Dictionary<K, V>) {
     for (k, v) in right {
         left.updateValue(v, forKey: k)
     }
+}
+func + <K, V>(left: Dictionary<K, V>, right: Dictionary<K, V>)
+    -> Dictionary<K, V>
+{
+    var map = Dictionary<K, V>()
+    for (k, v) in left {
+        map[k] = v
+    }
+    for (k, v) in right {
+        map[k] = v
+    }
+    return map
 }
 
 extension Dictionary {
