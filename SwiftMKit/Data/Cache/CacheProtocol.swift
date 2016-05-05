24d9e23da8307ddcecbedcf061c3d768182a92a2
cdts_change <cdts_change@aliyun.com>
2016-05-05 11:20:05 +0800

    单元测试

--- SwiftMKit/Data/Cache/CacheProtocol.swift
+++ SwiftMKit/Data/Cache/CacheProtocol.swift
@@ -9,12 +9,35 @@
 import Foundation
 import UIKit
 
-public protocol CacheProtocol {
-    func addObject(name: String, data: NSData) -> String
-    func addObject(name: String, image: UIImage) -> String
-    func addObject(name: String, filePath: String) -> String
+
+public protocol CacheModelProtocol {
+    var key: String { get }
+    var name: String { get set }
+    var filePath: NSURL { get }
+    var size: Double { get }
+    var mimeType : String { get set }
+    var createTime: NSTimeInterval { get }
+    var lastVisitTime: NSTimeInterval { get set }
+    var expireTime: NSTimeInterval { get set }
     
-    func objectForName(name: String) -> NSData?
-    func imageForName(name: String) -> UIImage?
-    func clear()
+}
+public protocol CachePoolProtocol {
+    var capacity: Double { get }
+    var size: Double { get set }
+    var basePath: NSURL { get set }
+    var name: String { get set }
+    
+    func addData(data: NSData, name: String?) -> String
+    func addImage(image: UIImage, name: String?) -> String
+    func addFile(filePath: NSURL, name: String?) -> String
+    
+    func getData(key: String) -> NSData?
+    func getImage(key: String) -> UIImage?
+    
+    func removeData(key: String) -> Bool
+    func removeImage(key: String) -> Bool
+    
+    func all() -> [CacheModelProtocol]?
+    
+    func clear() -> Bool
 }
