7d0e0ec9a6bd1a57c2f684b03c90639bf2d5e73c
cheny <yong.chen@jimubox.com>
2016-05-11 13:39:51 +0800

    提交代码

--- SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUISideViewController.swift
+++ SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUISideViewController.swift
@@ -7,7 +7,20 @@
 //
 
 import UIKit
+import CocoaLumberjack
 
 class MKUISideViewController: BaseViewController {
-
+    private var _viewModel = MKUISideViewModel()
+    override var viewModel: BaseKitViewModel!{
+        get { return _viewModel }
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        title = "侧滑视图"
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        DDLogInfo("点击了屏幕")
+    }
 }
