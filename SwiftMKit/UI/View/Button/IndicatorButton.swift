d88e0d01e7e7da2957c107e1a765a59afc8610c9
cheny <yong.chen@jimubox.com>
2016-05-18 11:02:28 +0800

    IndicatorButton 初始化版本

--- SwiftMKit/UI/View/Button/IndicatorButton.swift
+++ SwiftMKit/UI/View/Button/IndicatorButton.swift
@@ -9,13 +9,107 @@
 import UIKit
 
 public class IndicatorButton: UIButton {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    // MARK: - 可视化编辑属性
+    /// 标识是否是向下切换title
+    var upToDown: Bool = false
+    /// borderColor
+    var borderColor: UIColor = UIColor.clearColor() {
+        didSet {
+            layer.borderColor = borderColor.CGColor
+        }
     }
-    */
-
+    /// borderWidth
+    var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    /// cornerRadius
+    var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+//    public override var enabled: Bool {
+//        didSet {
+//            if oldValue != enabled {
+//                if oldValue {
+//                    
+//                }
+//            }
+//        }
+//    }
+    
+    // MARK: - 私有属性
+    lazy var backView = UIView()
+    lazy var lblMessage = UILabel()
+    lazy var indicatorView = UIActivityIndicatorView()
+    
+    // MARK: - 构造方法
+    required public init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+        setup()
+    }
+    
+    init() {
+        super.init(frame: CGRectZero)
+        setup()
+    }
+    
+    // MARK: - 公开方法
+    func ib_loadingWithTitle(title: String) {
+        // TODO: 如果title和旧title相同  不需要显示动画滚动
+        // TODO: lblMessage 和 indicatorView 的位置
+        enabled = false
+        lblMessage.text = title
+        lblMessage.sizeToFit()
+        lblMessage.center = CGPointMake(self.w * 0.5, self.h * 0.5)
+        
+        indicatorView.centerY = lblMessage.centerY
+        indicatorView.left = lblMessage.left - 20 - indicatorView.w
+        indicatorView.startAnimating()
+        // TODO: 滚动方向
+        backView.transform = CGAffineTransformMakeTranslation(0, self.h)
+        UIView.animateWithDuration(0.5) {
+            self.titleLabel!.alpha = 0
+            self.backView.alpha = 1
+            self.backView.transform = CGAffineTransformIdentity
+        }
+    }
+    
+    func ib_resetToNormalState() {
+        enabled = true
+        UIView.animateWithDuration(0.5, animations: {
+            self.titleLabel!.alpha = 1
+            self.backView.alpha = 0
+            // TODO: 滚动方向
+            self.backView.transform = CGAffineTransformMakeTranslation(0, self.h)
+        }) { (finished) in
+            self.backView.transform = CGAffineTransformIdentity
+            self.indicatorView.stopAnimating()
+        }
+    }
+    
+    // MARK: - 私有方法
+    private func setup() {
+        // 初始化按钮
+        titleLabel?.font = UIFont.systemFontOfSize(14)
+        backgroundColor = UIColor.blueColor()
+        layer.masksToBounds = true
+        // 初始化backView及其子视图
+        lblMessage.textColor = titleLabel?.textColor
+        lblMessage.font = titleLabel?.font
+        backView.addSubview(lblMessage)
+        
+        indicatorView.activityIndicatorViewStyle = .White
+        indicatorView.hidesWhenStopped = true
+        backView.addSubview(indicatorView)
+        
+        backView.frame = CGRectMake(0, 0, self.w, self.h)
+        backView.backgroundColor = UIColor.clearColor()
+        backView.alpha = 0
+        
+        addSubview(backView)
+    }
 }
--- SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUIIndicatorButtonViewController.swift
+++ SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUIIndicatorButtonViewController.swift
@@ -7,8 +7,20 @@
 //
 
 import UIKit
+import CocoaLumberjack
 
 class MKUIIndicatorButtonViewController: BaseViewController {
 
     @IBOutlet weak var btnTest: IndicatorButton!
+    
+    override func setupUI() {
+        super.setupUI()
+        
+        btnTest.cornerRadius = 3
+        btnTest.upToDown = false
+        btnTest.rac_signalForControlEvents(.TouchUpInside).subscribeNext { (sender) in
+            DDLogInfo("\(sender)")
+            self.btnTest.ib_loadingWithTitle("正在拼命加载中...")
+        }
+    }
 }
