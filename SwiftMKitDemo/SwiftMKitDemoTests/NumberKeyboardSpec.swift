afa1eeceb589db2a121cd3792eca616ea94c4b83
王燕燕 <yanyan.wang@jimubox.com>
2016-05-19 16:40:42 +0800

    添加单元测试

---
+++ SwiftMKitDemo/SwiftMKitDemoTests/NumberKeyboardSpec.swift
@@ -0,0 +1,32 @@
+//
+//  NumberKeyboardSpec.swift
+//  SwiftMKitDemo
+//
+//  Created by jimubox on 16/5/19.
+//  Copyright © 2016年 cdts. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+@testable import SwiftMKitDemo
+
+class NumberKeyboardSpec: QuickSpec {
+    
+    
+    override func spec() {
+//        describe("clear cache"){
+//            let cachePool: CachePoolProtocol = CachePool()
+//            context("clear will be success"){
+//                let isCleared = cachePool.clear()
+//                it("return true"){
+//                    expect(isCleared).to(beTrue())
+//                }
+//                it("cache size should be equal to capacity"){
+//                    expect(cachePool.size).to(equal(0))
+//                }
+//            }
+//        }
+    }
+    
+}
