5675e2e472f6c7cf94706e3ebf7934d5de6a16d1
cheny <yong.chen@jimubox.com>
2016-05-20 10:42:56 +0800

    GalaryCollectionViewCell布局

--- SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUIGalaryCollectionViewController.swift
+++ SwiftMKitDemo/SwiftMKitDemo/UI/UI/View/MKUIGalaryCollectionViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class MKUIGalaryCollectionViewController: BaseListViewController {
+    @IBOutlet weak var tableView: UITableView!
 
+    override var listView: UIScrollView! {
+        get { return tableView }
+    }
+    
+    private var _viewModel = MKUIGalaryCollectionViewModel()
+    override var viewModel: BaseKitViewModel!{
+        get { return _viewModel }
+    }
 }
 
 class GalaryCollectionViewController: UICollectionViewController {
     
     @IBOutlet weak var layout: GalaryLinearLayout!
+}
+
+class GalaryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lblDay: UILabel!
 }
