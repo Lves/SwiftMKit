f7e09c8f30a98edbea4b9eb0658f3204b46a932a
cdts_change <cdts_change@aliyun.com>
2016-05-23 17:30:16 +0800

    修改bug

--- SwiftMKit/Network/NetApiProtocol.swift
+++ SwiftMKit/Network/NetApiProtocol.swift
@@ -22,6 +22,7 @@ public protocol NetApiIndicatorProtocol : class {
 }
 
 public protocol NetApiProtocol: NetApiIndicatorProtocol {
+    var error: NetError? { get }
     var query: [String: AnyObject]? { get }
     var method: Alamofire.Method? { get }
     var url: String? { get }
@@ -37,6 +38,7 @@ public protocol NetApiProtocol: NetApiIndicatorProtocol {
 }
 
 public class NetApiAbstract: NetApiProtocol{
+    public var error: NetError?
     public var query: [String: AnyObject]?
     public var method: Alamofire.Method?
     public var url: String?
@@ -49,17 +51,28 @@ public class NetApiAbstract: NetApiProtocol{
     public var indicatorText: String?
     public var indicatorList: IndicatorListProtocol?
     
+    public convenience init(error: NetError) {
+        self.init()
+        self.error = error
+    }
+    public init() {
+    }
     public func fillJSON(json: AnyObject) {}
     public func transferURLRequest(request:NSMutableURLRequest) -> NSMutableURLRequest { return request }
     public func transferResponseJSON(response: Response<AnyObject, NSError>) -> Response<AnyObject, NSError> { return response }
     public func transferResponseData(response: Response<NSData, NSError>) -> Response<NSData, NSError> { return response }
     public func transferResponseString(response: Response<String, NSError>) -> Response<String, NSError> { return response }
-
+    
 }
 
 public extension NetApiProtocol {
     
     func signal(format:ApiFormatType = .JSON) -> SignalProducer<Self, NetError> {
+        if let err = error {
+            return SignalProducer { sink, _ in
+                sink.sendFailed(err)
+            }
+        }
         switch format {
         case .JSON:
             return NetApiData(api: self).requestJSON().map { _ in
