9895c29453e5b66bdb3f1a96b8095ddd5fdbf5f1
cdts_change <cdts_change@aliyun.com>
2016-05-25 00:46:05 +0800

    修改vc

--- SwiftMKit/Extension/UIViewController+Extension.swift
+++ SwiftMKit/Extension/UIViewController+Extension.swift
@@ -34,7 +34,7 @@ public extension UIViewController {
 
 //Route
 public extension UIViewController {
-    public func routeToName(name: String, params nextParams: Dictionary<String, AnyObject> = [:], storyboardName: String? = "", pop: Bool = false) {
+    public func routeToName(name: String, params nextParams: Dictionary<String, AnyObject> = [:], storyboardName: String? = "", pop: Bool = false) -> Bool {
         DDLogInfo("Route name: \(name) (\(nextParams.stringFromHttpParameters()))")
         if let vc = initialedViewController(name, params: nextParams, storyboardName: storyboardName) {
             if pop {
@@ -42,10 +42,13 @@ public extension UIViewController {
             } else {
                 self.navigationController?.pushViewController(vc, animated: true)
             }
+            return true
         } else if canPerformSegueWithIdentifier(name){
             self.performSegueWithIdentifier(name, sender: ["params": nextParams])
+            return true
         } else {
             DDLogError("Can't route to: \(name), please check the name")
+            return false
         }
     }
     public func initialedViewController(name: String, params nextParams: Dictionary<String, AnyObject> = [:], storyboardName: String? = "") -> UIViewController? {
