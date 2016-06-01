e7f7a2cf2a7deddca51c7be280449b2cade3cb40
cdts_change <cdts_change@aliyun.com>
2016-06-01 11:54:39 +0800

    登录 ui

--- SwiftMKit/Extension/UITextField+Extension.swift
+++ SwiftMKit/Extension/UITextField+Extension.swift
@@ -14,4 +14,12 @@ public extension UITextField {
     func rac_textSignalProducer() -> SignalProducer<String, NoError> {
         return self.rac_textSignal().toSignalProducer().map { $0 as! String }.flatMapError { _ in SignalProducer.empty }
     }
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSForegroundColorAttributeName: newValue!])
+        }
+    }
 }
