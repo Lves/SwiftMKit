fa885be985b70e7dffaa40d8119a183d284d18d5
cdts_change <cdts_change@aliyun.com>
2016-06-08 11:29:22 +0800

    银行卡format

--- SwiftMKit/Extension/UITextField+Extension.swift
+++ SwiftMKit/Extension/UITextField+Extension.swift
@@ -11,15 +11,57 @@ import ReactiveCocoa
 import Result
 
 public extension UITextField {
-    func rac_textSignalProducer() -> SignalProducer<String, NoError> {
+    public func rac_textSignalProducer() -> SignalProducer<String, NoError> {
         return self.rac_textSignal().toSignalProducer().map { $0 as! String }.flatMapError { _ in SignalProducer.empty }
     }
-    @IBInspectable var placeHolderColor: UIColor? {
+    @IBInspectable public var placeHolderColor: UIColor? {
         get {
             return self.placeHolderColor
         }
         set {
             self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSForegroundColorAttributeName: newValue!])
         }
+    }
+    
+    public func reformatCardNumber(range: NSRange, replaceString string: String) -> Bool {
+        //Range.Lenth will greater than 0 if user is deleting text - Allow it to replce
+        if range.length > 0
+        {
+            return true
+        }
+        
+        //Dont allow empty strings
+        if string == " "
+        {
+            return false
+        }
+        
+        //Check for max length including the spacers we added
+        if range.location == 20
+        {
+            return false
+        }
+        
+        var originalText = self.text
+        let replacementText = string.stringByReplacingOccurrencesOfString(" ", withString: "")
+        
+        //Verify entered text is a numeric value
+        let digits = NSCharacterSet.decimalDigitCharacterSet()
+        for char in replacementText.unicodeScalars
+        {
+            if !digits.longCharacterIsMember(char.value)
+            {
+                return false
+            }
+        }
+        
+        //Put an empty space after every 4 places
+        if originalText!.length % 5 == 0
+        {
+            originalText?.appendContentsOf(" ")
+            self.text = originalText
+        }
+        return true
+
     }
 }
