370b40320164b83412c45934b0f4c0b7c012bbc6
wzs <zhanshi.wang@limiku.com>
2016-06-29 09:45:09 +0800

    揭换界面 阴影延迟隐藏问题

--- SwiftMKit/UI/View/Side/SideMenu.swift
+++ SwiftMKit/UI/View/Side/SideMenu.swift
@@ -186,6 +186,7 @@ public class SideMenu: UIViewController, UIGestureRecognizerDelegate {
     }
     public func hideMenu() {
         menuShowed.value = false
+        self.shadowView.hidden = true
         UIView.animateWithDuration(animationDuration, animations: {
             self.coverView.backgroundColor = UIColor.clearColor()
             switch self.direction {
@@ -198,7 +199,6 @@ public class SideMenu: UIViewController, UIGestureRecognizerDelegate {
         }) { _ in
             self.menuViewController?.view.removeFromSuperview()
             self.coverView.removeFromSuperview()
-            self.shadowView.hidden = true
         }
     }
     
