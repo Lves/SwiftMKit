b0f4035f22484bdcd1ea10de56df9b26b3a2f1e4
cdts_change <cdts_change@aliyun.com>
2016-07-08 15:38:13 +0800

    修改推送

---
+++ SwiftMKit/Data/Push/PushManager.swift
@@ -0,0 +1,40 @@
+//
+//  PushManager.swift
+//  Merak
+//
+//  Created by Mao on 7/8/16.
+//  Copyright © 2016 jimubox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CocoaLumberjack
+
+public class PushManager: NSObject {
+    private override init() {
+    }
+    public static let shared = PushManager()
+    public var pushUserInfo: [NSObject : AnyObject]?
+    public func showPushAlert() {
+        if let userInfo = pushUserInfo {
+            DDLogInfo("[PushManager] 收到推送消息")
+            DDLogInfo("[PushManager] \(pushUserInfo)")
+            let title = "消息推送"
+            let message : String? = userInfo["aps"]["alert"]
+            if let message = message {
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "我知道了", style: .Cancel, handler: nil))
+                if let vc = UIViewController.topController {
+                    if shouldShowPushAlertInViewController(vc) {
+                        vc.showAlert(alert, completion: nil)
+                        pushUserInfo = nil
+                    }
+                }
+            }
+            
+        }
+    }
+    public func shouldShowPushAlertInViewController(vc: UIViewController) -> Bool {
+        return true
+    }
+}
