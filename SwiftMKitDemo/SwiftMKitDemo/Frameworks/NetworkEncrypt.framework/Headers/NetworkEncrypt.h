fc7308c9a85e911dd14186a4bb2dbf37ca66f287
wei.mao <wei.mao@jimubox.com>
2016-10-19 14:25:00 +0800

    framework路径修改

---
+++ SwiftMKitDemo/SwiftMKitDemo/Frameworks/NetworkEncrypt.framework/Headers/NetworkEncrypt.h
@@ -0,0 +1,21 @@
+//
+//  NetworkEncrypt.h
+//  NetworkEncrypt
+//
+//  Created by shoron on 9/27/16.
+//  Copyright © 2016 PIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EncryptedNetworkManager.h"
+
+//! Project version number for NetworkEncrypt.
+FOUNDATION_EXPORT double NetworkEncryptVersionNumber;
+
+//! Project version string for NetworkEncrypt.
+FOUNDATION_EXPORT const unsigned char NetworkEncryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkEncrypt/PublicHeader.h>
+
+
