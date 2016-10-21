f3bc8aef9955d8e1a97ab0a4b57ee5a80ea8c18a
wzs <zhanshi.wang@limiku.com>
2016-10-21 14:30:48 +0800

    恢复加密代码 开关先关闭

--- SwiftMKit/Network/Encrypt/EncryptNetworkManager.swift
+++ SwiftMKit/Network/Encrypt/EncryptNetworkManager.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-//import NetworkEncrypt
+import NetworkEncrypt
 
 public enum NetworkEncryptStatusCode: Int {
     case UpgradeApp = 9000
@@ -21,12 +21,12 @@ public class EncryptNetworkManager: NSObject {
     public static let shared = EncryptNetworkManager()
     
     public func initEncryptFramework(appId: String, vId: String) {
-//        EncryptedNetworkManager.sharedEncryptedNetworkManager().setAppId(appId, vId: vId)
+        EncryptedNetworkManager.sharedEncryptedNetworkManager().setAppId(appId, vId: vId)
     }
     
     func disableEncrypt() {
         EncryptedRequest.disableEncrypt = true;
-//        self.encryptedTimer = NSTimer(timeInterval: DisableEncryptTime, target: self, selector: #selector(EncryptNetworkManager.enableEncrypt), userInfo: nil, repeats: false);
+        self.encryptedTimer = NSTimer(timeInterval: DisableEncryptTime, target: self, selector: #selector(EncryptNetworkManager.enableEncrypt), userInfo: nil, repeats: false);
     }
     
     func enableEncrypt() {
