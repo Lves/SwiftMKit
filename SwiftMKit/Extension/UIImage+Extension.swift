b5888e418ac68adb2c4f9defb21f409608457a0b
王燕燕 <yanyan.wang@jimubox.com>
2016-11-01 17:21:14 +0800

    图片压缩

--- SwiftMKit/Extension/UIImage+Extension.swift
+++ SwiftMKit/Extension/UIImage+Extension.swift
@@ -75,47 +75,91 @@ public extension UIImage {
         
     }
     
-    
     /**
      压缩图片
      */
     func restrict(maxWidth maxWidth: CGFloat, maxHeight: CGFloat, maxSize: Int) -> NSData? {
-        //先调整分辨率
-        var newSize = CGSize(width: self.size.width, height: self.size.height)
-        
-        let tempHeight = newSize.height / maxHeight
-        let tempWidth  = newSize.width / maxWidth
-        
-        if tempWidth > 1.0 && tempWidth > tempHeight {
-            newSize = CGSize(width: maxWidth, height: self.size.height / tempWidth)
-        }
-        else if tempHeight > 1.0 && tempWidth < tempHeight {
-            newSize = CGSize(width: self.size.width / tempHeight, height: maxHeight)
-        }
-        
-        UIGraphicsBeginImageContext(newSize)
-        self.drawAsPatternInRect(CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return bSearch(newImage, count: 10, maxSize: maxSize)
+        let newImage = resizeImage(self, maxWidth: maxWidth, maxHeight: maxHeight)
+//        return bSearch(newImage, count: 10, maxSize: maxSize)
+        return compressImageSize(newImage)
     }
     
-    func bSearch(image: UIImage, count: Int, maxSize: Int) -> NSData? {
-        var low = CGFloat(0)
-        var high = CGFloat(count)
+    func resizeImage(originalImg:UIImage, maxWidth: CGFloat, maxHeight: CGFloat) -> UIImage{
+        //prepare constants
+        let width = originalImg.size.width
+        let height = originalImg.size.height
+        let scale = width/height
+        print("原始长: \(height), 原始宽: \(width)")
+        var sizeChange = CGSize()
         
-        var mid = (low + high) / 2
-        while high > low {
-            if let imageData = UIImageJPEGRepresentation(image, mid/CGFloat(count)) {
-                if Int(Int64(imageData.length)) > maxSize {
-                    high = mid
-                } else {
-                    low = mid + 1
+        if width <= maxWidth && height <= maxHeight{ //a，图片宽或者高均小于或等于1280时图片尺寸保持不变，不改变图片大小
+            return originalImg
+        } else if width > maxWidth || height > maxHeight {//b,宽或者高大于1280，但是图片宽度高度比小于或等于2，则将图片宽或者高取大的等比压缩至1280
+            
+            if scale <= 2 && scale >= 1 {
+                let changedWidth:CGFloat = maxWidth
+                let changedheight:CGFloat = changedWidth / scale
+                sizeChange = CGSize(width: changedWidth, height: changedheight)
+                
+            } else if scale >= 0.5 && scale <= 1 {
+                
+                let changedheight:CGFloat = maxHeight
+                let changedWidth:CGFloat = changedheight * scale
+                sizeChange = CGSize(width: changedWidth, height: changedheight)
+                
+            } else if width > maxWidth && height > maxHeight {//宽以及高均大于1280，但是图片宽高比大于2时，则宽或者高取小的等比压缩至1280
+                
+                if scale > 2 {//高的值比较小
+                    
+                    let changedheight:CGFloat = maxHeight
+                    let changedWidth:CGFloat = changedheight * scale
+                    sizeChange = CGSize(width: changedWidth, height: changedheight)
+                    
+                } else if scale < 0.5{//宽的值比较小
+                    
+                    let changedWidth:CGFloat = maxWidth
+                    let changedheight:CGFloat = changedWidth / scale
+                    sizeChange = CGSize(width: changedWidth, height: changedheight)
+                    
                 }
-                mid = (low + high) / 2
+            } else {//d, 宽或者高，只有一个大于1280，并且宽高比超过2，不改变图片大小
+                return originalImg
             }
         }
-        return UIImageJPEGRepresentation(image, mid/CGFloat(count))
+        UIGraphicsBeginImageContext(sizeChange)
+        //draw resized image on Context
+        originalImg.drawInRect(CGRectMake(0, 0, sizeChange.width, sizeChange.height))
+        //create UIImage
+        let resizedImg = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        print("压缩后长: \(sizeChange.height), 压缩后宽: \(sizeChange.width)")
+        return resizedImg
     }
+    
+    //图片质量压缩
+    func compressImageSize(image:UIImage, maxSize: Int) -> NSData{
+        
+        var zipImageData = UIImageJPEGRepresentation(image, 1.0)!
+        let originalImgSize = zipImageData.length/1024 as Int  //获取图片大小
+        print("原始大小: \(originalImgSize)")
+        if originalImgSize > 3000 {
+            zipImageData = UIImageJPEGRepresentation(image,0.2)!
+            if zipImageData.length/1024 > maxSize {
+                zipImageData = UIImageJPEGRepresentation(zipImageData, 0.99)!
+            }
+        } else if originalImgSize>1500 {
+            zipImageData = UIImageJPEGRepresentation(image,0.5)!
+            if zipImageData.length/1024 > maxSize {
+                zipImageData = UIImageJPEGRepresentation(zipImageData, 0.99)!
+            }
+        } else {
+            zipImageData = UIImageJPEGRepresentation(image,0.7)!
+            if zipImageData.length/1024 > maxSize {
+                zipImageData = UIImageJPEGRepresentation(zipImageData, 0.99)!
+            }
+        }
+        print("上传大小: \(zipImageData.length/1024)")
+        return zipImageData
+    }
+    
 }
