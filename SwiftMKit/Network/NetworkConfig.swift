200f3c5c0aaf96bdfb3408f37e8a61354b8ce5cb
LiXingLe <xingle.li@jimubox.com>
2016-11-04 14:43:03 +0800

    环境

--- SwiftMKit/Network/NetworkConfig.swift
+++ SwiftMKit/Network/NetworkConfig.swift
@@ -10,7 +10,7 @@ import Foundation
 import PINCache
 
 public enum Environment: Int {
-    case Local,Dev,Gray,PreProduct,Product,Custom
+    case Local,Dev,Gray,PreProduct,Product,Custom,LoanDev
 }
 
 public class NetworkConfig {
