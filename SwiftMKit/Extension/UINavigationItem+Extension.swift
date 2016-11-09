41b5f60b68637071e4067ce1585dd1f666289a9a
cheny <yong.chen@jimubox.com>
2016-11-09 17:41:55 +0800

    add file

---
+++ SwiftMKit/Extension/UINavigationItem+Extension.swift
@@ -0,0 +1,12 @@
+
+//
+//  File: UINavigationItem+Extension.swift
+//  Project: Fund
+//  Description:
+//  Author: cheny
+//  Date: 16/11/9
+//  Version:
+//  Copyright © 2016年 pintec. All rights reserved.
+//
+
+import UIKit
