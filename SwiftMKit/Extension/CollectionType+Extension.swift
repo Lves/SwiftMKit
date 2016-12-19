85d9401b1b9fdfccc4e2c357e0e0e29649ccf52d
wzs <zhanshi.wang@limiku.com>
2016-12-19 11:03:31 +0800

    Collection safe index

--- SwiftMKit/Extension/CollectionType+Extension.swift
+++ SwiftMKit/Extension/CollectionType+Extension.swift
@@ -9,6 +9,10 @@
 import Foundation
 import CocoaLumberjack
 
+/*
+ * swift 2.3
+ */
+/*
 extension Collection {
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
     subscript (safe index: Index) -> Iterator.Element? {
@@ -18,5 +22,22 @@ extension Collection {
             DDLogError("Collection Type Out of bounds with: \(index)/\(indices.count)")
             return nil
         }
+    }
+}
+*/
+
+/*
+ * swift 3.0
+ */
+extension Collection {
+    subscript (safe index: Index) -> Iterator.Element? {
+        return (startIndex..<endIndex).contains(index) ? self[index] : nil
+    }
+}
+
+extension Collection
+    where Indices.Iterator.Element: Equatable, Index == Indices.Iterator.Element{
+    subscript (safe index: Indices.Iterator.Element) -> Iterator.Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
