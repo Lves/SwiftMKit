9009d0359bba6c0e7a10d50979b449c241b4bc57
wei.mao <wei.mao@jimubox.com>
2017-01-04 19:15:11 +0800

    textfield limit length

--- SwiftMKit/UI/View/TextField/LimitLengthTextField.swift
+++ SwiftMKit/UI/View/TextField/LimitLengthTextField.swift
@@ -13,12 +13,12 @@ public class UITextFieldMaxLengthObserver: NSObject {
     public func textChanged(textField: UITextField) {
         let destText = textField.text
         let maxLength = textField.maxLength
-        // 对中文的特殊处理，shouldChangeCharactersInRangeWithReplacementString 并不响应中文输入法的选择事件
-        // 如拼音输入时，拼音字母处于选中状态，此时不判断是否超长
-        let selectedRange = textField.markedTextRange
-        if selectedRange != nil {
-            if let destText = destText {
-                if (destText.length ?? 0) > maxLength {
+        if let destText = destText {
+            // 对中文的特殊处理，shouldChangeCharactersInRangeWithReplacementString 并不响应中文输入法的选择事件
+            // 如拼音输入时，拼音字母处于选中状态，此时不判断是否超长
+            let selectedRange = textField.markedTextRange
+            if selectedRange == nil || selectedRange!.start == selectedRange!.end {
+                if destText.length > maxLength {
                     textField.text = destText.substringToIndex(destText.startIndex.advancedBy(maxLength))
                 }
             }
