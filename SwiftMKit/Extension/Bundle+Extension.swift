fc16e0a1bd9850061eea2b3cf92df969b27159f7
lveslxl@gmail.com <lveslxl@gmail.com>
2018-08-16 18:33:10 +0800

    创建tag解决资源问题

---
+++ SwiftMKit/Extension/Bundle+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Bundle+Extension.swift
+//  SwiftMKit
+//
+//  Created by lixingle on 2018/8/16.
+//  Copyright © 2018年 cdts. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftMKitDemo/SwiftMKit/SwiftMKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMKit.h
+//  SwiftMKit
+//
+//  Created by lixingle on 2018/8/16.
+//  Copyright © 2018年 cdts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMKit.
+FOUNDATION_EXPORT double SwiftMKitVersionNumber;
+
+//! Project version string for SwiftMKit.
+FOUNDATION_EXPORT const unsigned char SwiftMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMKit/PublicHeader.h>
+
+
